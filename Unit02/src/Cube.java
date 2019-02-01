8a054721572d9020d11eb337c562af12601a0f8d
Derek-Wen <derek.hz.wen@gmail.com>
2019-02-01 12:52:27 -0800

    Moved Files And Modified Code

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Derek Wen
+//Date - 2/1/2019
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		double SA = 0;
+		SA = (int) 6 * side * side;
+		return SA;
+	}
+}
---
+++ Unit02/src/Trap.java
@@ -0,0 +1,13 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Derek Wen
+//Date - 2/1/2019
+//Class - APCSA
+//Lab  - Unit 2, Trap,
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return 0;
+	}
+}
