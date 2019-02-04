2f87cb4491d25b515c0f5a7a0ff8b1cb4e8dca6a
Derek-Wen <derek.hz.wen@gmail.com>
2019-02-04 11:47:45 -0800

    working on Unit 3

--- Unit03/src/Distance.java
+++ Unit03/src/Distance.java
@@ -14,31 +14,45 @@ public class Distance
 {
 	private int xOne,yOne,xTwo,yTwo;
 	private double distance;
-
+	Scanner keyboard = new Scanner(System.in);
 	public Distance()
 	{
-		
+		xOne = 0;
+		yOne = 0;
+		xTwo = 0;
+		yTwo = 0;
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
-	public void calcDistance()
+	public void calcDistance(int x1,int x2,int y1,int y2)
 	{
+		distance=Math.sqrt(Math.pow((xTwo - xOne),2) + Math.pow((yTwo - yOne), 2));
 	}
 	
 	public double getDistance()
 	{
-		return 0.0;
+		return "0.0";
+		
 	}
 	
 	public void print()
 	{
+		System.out.println("distance ==" +distance);
 	}
 	
 	//complete print or the toString
