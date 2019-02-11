efc9b8a0648045459e7f3b57dd4c8a974f9ef85f
Derek-Wen <derek.hz.wen@gmail.com>
2019-02-11 12:43:07 -0800

    Finished String Runner

--- Unit05/src/StringRunner.java
+++ Unit05/src/StringRunner.java
@@ -13,5 +13,12 @@ public class StringRunner
 {
 	public static void main ( String[] args )
 	{
+		Scanner keyboard = new Scanner(System.in);
+		StringOddOrEven input = new StringOddOrEven("cat");
+		out.print("Enter a word :: ");
+		String check = keyboard.nextLine();
+		input.setString(check);
+		input.isEven();
+		out.print(input.toString());
 	}
 }
