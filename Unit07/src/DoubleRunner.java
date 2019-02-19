2dc04353d1cbe7562d969b05f09fcf47cae9cf7a
Derek-Wen <derek.hz.wen@gmail.com>
2019-02-19 12:46:50 -0800

    Finished BiggestDouble

--- Unit07/src/DoubleRunner.java
+++ Unit07/src/DoubleRunner.java
@@ -3,16 +3,26 @@
 //Name -  
 
 import static java.lang.System.*;
-
+import java.util.Scanner;
 public class DoubleRunner
 {
 	public static void main(String[] args)
 	{
-		BiggestDouble run = new BiggestDouble(4.5,6.7,7.8,9.0);
-		System.out.println(run);
-		System.out.println("biggest = " + run.getBiggest() + "\n");
+		Scanner keyboard = new Scanner (System.in);
+		int x = 0;
+		double one = 0, two = 0, three = 0, four = 0;
+		while ( x < 6) {
 
-		//add more test cases
-				
+			System.out.println("Enter number :: ");
+			one = keyboard.nextDouble();
+			System.out.println("Enter number :: ");
+			two = keyboard.nextDouble();
+			System.out.println("Enter number :: ");
+			three = keyboard.nextDouble();
+			System.out.println("Enter number :: ");
+			four = keyboard.nextDouble();
+			BiggestDouble run = new BiggestDouble(one, two, three, four);
+			System.out.println(run);
+		}
 	}
-}
+	}
