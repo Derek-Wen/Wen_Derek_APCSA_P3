7356b149b5a53a966e36c96c6036c97791103d8e
Derek-Wen <derek.hz.wen@gmail.com>
2019-03-12 13:28:27 -0700

    Worked on Doggies

--- Unit11/src/Doggies.java
+++ Unit11/src/Doggies.java
@@ -10,24 +10,45 @@ public class Doggies
 
 	public Doggies(int size)
 	{
+		pups = new Dog[size];
 		//point pups at a new arry of Dog
 		
 	}
 	
 	public void set(int spot, int age, String name)
 	{
+		Dog var1 = new Dog(age, name);
+		if (0 < spot  && spot < pups.length -1) {
+			pups[spot] = var1;
+		}
 		//put a new Dog in the array at spot 
 		//make sure spot is in bounds		
 	}
 
 	public String getNameOfOldest()
 	{
+		int largest = 0;
+		String largestname="";
+		for (int i = 0; i < pups.length; i++) {
+			if (pups[i].getAge() > largest) {
+				largest = pups[i].getAge();
+				largestname= pups[i].getName();
+			}
+		}
 		return null;
 	}
 
 	public String getNameOfYoungest()
 	{
-		return null;
+		int smallest = 0;
+		String smallestname="";
+		for (int i = 0; i < pups.length; i++) {
+			if (pups[i].getAge() < smallest) {
+				smallest = pups[i].getAge();
+				smallestname = pups[i].getName();
+			}
+		}
+		return smallestname;
 	}
 
 	public String toString()
